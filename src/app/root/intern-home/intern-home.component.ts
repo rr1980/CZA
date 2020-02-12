1a3f6f7e519678a83438c4c3f951cfabf1175d15
rr1980 <rener1980@googlemail.com>
2020-02-12 19:20:44 +0100

    test

--- src/app/root/intern-home/intern-home.component.ts
+++ src/app/root/intern-home/intern-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InternHomeService, Employee } from './intern-home.service';
+import DataSource from 'devextreme/data/data_source';
 
 @Component({
   selector: 'cza-intern-home',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InternHomeComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(service: InternHomeService) {
+    this.dataSource = new DataSource({
+        store: service.getData()
+    });
+
+    this.resourcesDataSource = service.getEmployees();
+}
+
 
   ngOnInit() {
   }
@@ -16,4 +26,34 @@ export class InternHomeComponent implements OnInit {
     
   }
 
+  dataSource: any;
+  currentDate: Date = new Date(2016, 7, 2, 11, 30);
+  resourcesDataSource: Employee[];
+
+  markWeekEnd(cellData) {
+      function isWeekEnd(date) {
+          var day = date.getDay();
+          return day === 0 || day === 6;
+      }
+      var classObject = {};
+      classObject["employee-" + cellData.groups.employeeID] = true;
+      classObject['employee-weekend-' + cellData.groups.employeeID] = isWeekEnd(cellData.startDate)
+      return classObject;
+  }
+
+  markTraining(cellData) {
+      var classObject = {
+          "day-cell": true
+      }
+
+      classObject[InternHomeComponent.getCurrentTraining(cellData.startDate.getDate(), cellData.groups.employeeID)] = true;
+      return classObject;
+  }
+
+  static getCurrentTraining(date, employeeID) {
+      var result = (date + employeeID) % 3,
+          currentTraining = "training-background-" + result;
+
+      return currentTraining;
+  }
 }
--- src/app/root/intern-home/intern-home.module.ts
+++ src/app/root/intern-home/intern-home.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { InternHomeRoutingModule, routedComponents } from './intern-home-routing.module';
-import { DxButtonModule } from 'devextreme-angular';
+import { DxButtonModule, DxSchedulerModule, DxTemplateModule } from 'devextreme-angular';
 import { MatSliderModule } from '@angular/material/slider';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule,
     InternHomeRoutingModule,
     DxButtonModule,
+    DxSchedulerModule,
+    DxTemplateModule,
     MatSliderModule
   ],
   declarations: [routedComponents]
-}) 
+})
 export class InternHomeModule { }
