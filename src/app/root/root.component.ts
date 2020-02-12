346429e4d4dc0aba6759894eb2d6d066413e2f61
rr1980 <rener1980@googlemail.com>
2020-02-12 20:26:45 +0100

    1

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+import deMessages from "devextreme/localization/messages/de.json";
+import enMessages from "devextreme/localization/messages/en.json";
+import { formatMessage, locale, loadMessages } from "devextreme/localization";
+
+const customMessages = {
+  de: {
+    'greetingMessage': "Willkommen bei {0}"
+  },
+  en: {
+    'greetingMessage': "Welcome on {0}"
+  }
+}
+
 @Component({
   selector: 'cza-root',
   templateUrl: 'root.component.html',
   styleUrls: ['root.component.scss']
 })
 export class RootComponent {
-  title = 'CZA';
+  constructor() {
+    console.debug(navigator.language);
+    console.debug(deMessages);
+    loadMessages(deMessages);
+    loadMessages(enMessages);
+    loadMessages(customMessages);
+    locale(navigator.language);
+  }
+
+  get greeting() {
+    return formatMessage('greetingMessage', 'CZA');
+  }
+
+  goDE(){
+    locale('de-DE');
+  }
+
+  goEN(){
+    locale('en-GB');
+  }
 }
